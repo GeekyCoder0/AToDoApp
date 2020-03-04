e0a6fa84afe7b0705867d19f964970ab636ca8f9
Prakash Sohanlal <prakash.sohanlal@STS.CO.IN>
2020-03-04 16:22:53 +0530

    Updates

---
+++ precache-manifest.fbd36f41dfdf8e77db40a70ccb26305a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14959a9f720d73d77a9abe9d45aaae97",
+    "url": "/AToDoApp/index.html"
+  },
+  {
+    "revision": "598a887e10e8ab9247b8",
+    "url": "/AToDoApp/static/css/main.25a79f4f.chunk.css"
+  },
+  {
+    "revision": "242dc048db0ac32dd2d8",
+    "url": "/AToDoApp/static/js/2.5c233a3d.chunk.js"
+  },
+  {
+    "revision": "598a887e10e8ab9247b8",
+    "url": "/AToDoApp/static/js/main.d61f0685.chunk.js"
+  },
+  {
+    "revision": "a5c9bee201b3c870ba5c",
+    "url": "/AToDoApp/static/js/runtime~main.b41275e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AToDoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
